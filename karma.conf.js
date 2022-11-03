cc66647f984b99a4ff605372bf1e5aa563e9e211
Luis Javier Ortiz Leigh <93390352+JavierOrtiz777@users.noreply.github.com>
2022-11-02 19:26:21 -0500

    HU Hotels

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/activities-life-travel'),
+      dir: require('path').join(__dirname, './coverage/life-travel'),
       subdir: '.',
       reporters: [
         { type: 'html' },
